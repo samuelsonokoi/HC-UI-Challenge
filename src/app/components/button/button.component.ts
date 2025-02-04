32aa0e5b01fa3c3ea8a73616684e553165e847f0
Samuelson Okoi <samuelsonokoi@gmail.com>
2025-02-04 14:26:40 +0100

    completed steps 1 and 2

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-
+  @Input() showBackButton = true;
+  
 }
--- src/app/pages/buy/buy.component.ts
+++ src/app/pages/buy/buy.component.ts
@@ -1,20 +1,35 @@
-import { NgClass, NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { DecimalPipe, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-buy',
-  imports: [ReactiveFormsModule, NgClass, NgIf, NgFor],
+  imports: [ReactiveFormsModule, NgIf, NgFor, DecimalPipe, ButtonComponent],
   templateUrl: './buy.component.html',
   styleUrl: './buy.component.scss'
 })
-export class BuyComponent {
+export class BuyComponent implements OnInit {
   buyForm = new FormGroup({
-
+    amountToPay: new FormControl(100, [Validators.required, Validators.min(100)]),
+    accountName: new FormControl('Dominic Mulinda', [Validators.required]),
+    phoneNumber: new FormControl('713210124', [Validators.required])
   });
-  currentStep = 1;
+  currentStep = 2;
+  amountToReceive = 0;
+  kenyanRate = 128.152;
+
+  ngOnInit(): void {
+    this.amountToReceive = 100 * this.kenyanRate;
+  }
 
   onSubmit(){
 
   }
+
+  calculateAmountToReceive = (event: Event | any) => this.amountToReceive = parseInt(event.target.value) * this.kenyanRate;
+
+  moveForward = () => this.currentStep = this.currentStep < 6 ? this.currentStep + 1 : this.currentStep
+
+  moveBackward = () => this.currentStep = this.currentStep > 1 ? this.currentStep - 1 : this.currentStep
 }
