398207e2b06ad9719232077fd58600c4a91b6e86
Samuelson Okoi <samuelsonokoi@gmail.com>
2025-02-04 10:41:12 +0100

    added footer and logo

--- src/app/pages/buy/buy.component.ts
+++ src/app/pages/buy/buy.component.ts
@@ -1,11 +1,20 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-buy',
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass, NgIf, NgFor],
   templateUrl: './buy.component.html',
   styleUrl: './buy.component.scss'
 })
 export class BuyComponent {
+  buyForm = new FormGroup({
 
+  });
+  currentStep = 1;
+
+  onSubmit(){
+
+  }
 }
